644c3985df9fcbd1f6811501ed66c7587fbea59a
namde <namdes48@gmail.com>
2022-06-15 17:29:51 +0700

    acc

--- routes/user.api.js
+++ routes/user.api.js
@@ -3,5 +3,5 @@ var router = express.Router();
 var apiUser = require('../controllers/user.controller.api');
 router.post('/user/reg', apiUser.postRegister);
 router.post('/user/login', apiUser.postLogin);
-router.post('/user/getacc', apiUser.getAcc);
+
 module.exports = router;
